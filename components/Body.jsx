9760935a5f23935149d489d50e2677988796531b
rijalsawan <rijalsawan@gmail.com>
2024-11-21 20:18:47 -0600

    fix: update link in Body component to point to new bus schedule site

--- components/Body.jsx
+++ components/Body.jsx
@@ -590,7 +590,7 @@ const Body = () => {
                     </div>
                     <div className="flex justify-center gap-4">
                       <a
-                        href="https://simple-blog-silk.vercel.app/"
+                        href="https://bus-schedule-navy.vercel.app/"
                         
                         className="bg-white text-black px-4 py-2 rounded-full hover:bg-gray-300 transition-colors duration-300 ease-in-out"
                       >
