af836a86db310e7a14ba3abca89bca5cc49dc715
rijalsawan <rijalsawan@gmail.com>
2025-07-08 15:06:14 -0500

    feat: add @vercel/analytics for enhanced analytics tracking

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Analytics } from "@vercel/analytics/next"
 
 export default function App({ Component, pageProps }) {
   return (
@@ -10,7 +11,9 @@ export default function App({ Component, pageProps }) {
           enableSystem
           disableTransitionOnChange
         >
+        
     <Component {...pageProps} />
+    <Analytics />
         </ThemeProvider>
   </>
 );
